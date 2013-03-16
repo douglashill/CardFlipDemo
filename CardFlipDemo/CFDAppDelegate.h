49d44620c975ad6353c94be185cae3df86474ac6
Douglas Hill <hello@douglashill.co>
2013-03-16 10:33:28 +0000

    Initial Commit

---
+++ CardFlipDemo/CFDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CFDAppDelegate.h
+//  CardFlipDemo
+//
+//  Created by Douglas Hill on 16/03/2013.
+//  Copyright (c) 2013 Douglas Hill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CFDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CardFlipDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CardFlipDemo
+//
+//  Created by Douglas Hill on 16/03/2013.
+//  Copyright (c) 2013 Douglas Hill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CFDAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([CFDAppDelegate class]));
+	}
+}
